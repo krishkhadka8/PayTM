af30695e6e0b4398619f73bf5303603119e98c3d
Turbobot <turbobot@vercel.com>
2024-09-10 20:50:13 +0530

    feat(create-turbo): apply official-starter transform

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-
+import { PrismaClient } from "@repo/db/client";
 export default function Home() {
   return (
     <div className="text-2xl">
