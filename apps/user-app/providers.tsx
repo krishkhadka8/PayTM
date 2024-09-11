6c8f64a24fdacfb8a45adc806fa8761a7c9e5af4
krishna khadka <krishkhadka8@gmail.com>
2024-09-11 22:25:57 +0530

    added authentication

--- apps/user-app/providers.tsx
+++ apps/user-app/providers.tsx
@@ -1,8 +1,8 @@
 "use client"
 import { RecoilRoot } from "recoil";
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
-export const Providers = ({children} : {children: React.ReactNode})=>{
+export const Providers = ({children}: {children: React.ReactNode}) => {
     return <RecoilRoot>
         <SessionProvider>
             {children}
