8da064a34cefef3e862ba15fe041825363d5a5cd
krishna khadka <krishkhadka8@gmail.com>
2024-09-12 12:24:01 +0530

    Modified UI for user-app & merchant-app

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,10 +1,10 @@
-import  { getServerSession } from "next-auth"
-import { NextResponse } from "next/server"
+import { getServerSession } from "next-auth"
+import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
-export const GET = async () =< {
+export const GET = async () => {
     const session = await getServerSession(authOptions);
-    if(session.user) {
+    if (session.user) {
         return NextResponse.json({
             user: session.user
         })
